936a0cd4a27ec0dd44942b2337aa34bb791f9064
Raphael Freitas da Silva <o.raphaelfreitas@gmail.com>
2022-02-07 15:28:57 -0300

    format code

--- sql_app/database.py
+++ sql_app/database.py
@@ -7,7 +7,8 @@ SQLALCHEMY_DATABASE_URL = "sqlite:///./database.db"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 engine = create_engine(
-  SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False} # for sqlite only
+    SQLALCHEMY_DATABASE_URL, connect_args={
+        'check_same_thread': False}  # for sqlite only
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- sql_app/schemas.py
+++ sql_app/schemas.py
@@ -2,30 +2,36 @@ from typing import Optional
 
 from pydantic import BaseModel
 
+
 class ItemBase(BaseModel):
-  title: str
-  description: Optional[str] = None
+    title: str
+    description: Optional[str] = None
+
 
 class ItemCreate(ItemBase):
-  pass
+    pass
+
 
 class Item(ItemBase):
-  id: int
-  owner_id: int
+    id: int
+    owner_id: int
 
-  class Config:
-    orm_mode = True
+    class Config:
+        orm_mode = True
+
 
 class UserBase(BaseModel):
-  email: str
+    email: str
+
 
 class UserCreate(UserBase):
-  password: str
+    password: str
+
 
 class User(UserBase):
-  id: int
-  is_active: bool
-  items: list[Item] = []
+    id: int
+    is_active: bool
+    items: list[Item] = []
 
-  class Config:
-    orm_mode = True
+    class Config:
+        orm_mode = True
