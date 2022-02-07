01086fb01cc53e387d4bd7bbdebbce678207fc2a
Raphael Freitas da Silva <o.raphaelfreitas@gmail.com>
2022-02-07 14:45:34 -0300

    create simple crud

---
+++ sql_app/database.py
@@ -0,0 +1,15 @@
+from sqlite3 import connect
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./database.db"
+# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+
+engine = create_engine(
+  SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False} # for sqlite only
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
---
+++ sql_app/schemas.py
@@ -0,0 +1,28 @@
+from typing import Optional
+
+from pydantic import BaseModel
+
+class ItemBase(BaseModel):
+  title: str
+  description: Optional[str] = None
+
+class ItemCreate(ItemBase):
+  pass
+
+class Item(ItemBase):
+  id: int
+  owner_id: int
+
+  class Config:
+    orm_mode = True
+
+class UserBase(BaseModel):
+  email: str
+
+class User(UserBase):
+  id: int
+  is_active: bool
+  items: list[Item] = []
+
+  class Config:
+    orm_mode = True
